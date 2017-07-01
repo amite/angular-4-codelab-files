7be69ceba60bf17b6ecb5fd59e60186ee7e21ba7
Amit Erandole <amit.erandole@gmail.com>
2017-07-01 08:01:21 +0530

    added customer data for project

---
+++ customers.data.js
@@ -0,0 +1,71 @@
+
+const customers = [{
+    id: 13,
+    gender: "male",
+    firstName: "henrik",
+    lastName: "graf",
+    email: "henrik.graf@example.com",
+    phone: "0293-1064710",
+    picture: "https://randomuser.me/api/portraits/med/men/80.jpg",
+    nat: "DE"
+},
+{
+    id: 14,
+    gender: "female",
+    firstName: "begoña",
+    lastName: "santos",
+    email: "begoña.santos@example.com",
+    phone: "964-499-852",
+    picture: "https://randomuser.me/api/portraits/med/women/74.jpg",
+    nat: "ES"
+},
+{
+    id: 15,
+    gender: "female",
+    firstName: "terra",
+    lastName: "roberts",
+    email: "terra.roberts@example.com",
+    phone: "(073)-750-5367",
+    picture: "https://randomuser.me/api/portraits/women/15.jpg",
+    nat: "US"
+},
+{
+    id: 16,
+    gender: "male",
+    firstName: "tiemo",
+    lastName: "ham",
+    email: "tiemo.ham@example.com",
+    phone: "(397)-811-4084",
+    picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    nat: "NL"
+},
+{
+    id: 17,
+    gender: "female",
+    firstName: "colette",
+    lastName: "rootse",
+    email: "colette.vanrootselaar@example.com",
+    phone: "(014)-702-6708",
+    picture: "https://randomuser.me/api/portraits/med/women/77.jpg",
+    nat: "NL"
+},
+{
+    id: 18,
+    gender: "female",
+    firstName: "maya",
+    lastName: "knight",
+    email: "maya.knight@example.com",
+    phone: "544-665-3931",
+    picture: "https://randomuser.me/api/portraits/med/women/90.jpg",
+    nat: "CA"
+},
+{
+    id: 19,
+    gender: "male",
+    firstName: "clemens",
+    lastName: "popp",
+    email: "clemens.popp@example.com",
+    phone: "0624-0042551",
+    picture: "https://randomuser.me/api/portraits/med/men/53.jpg",
+    nat: "DE"
+}]
